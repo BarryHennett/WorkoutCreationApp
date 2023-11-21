b9cacf8f221299e761f9836b4d0f5a99c6acff86
BarryHennett <124011454+BarryHennett@users.noreply.github.com>
2023-11-21 14:56:27 +1300

    20 mins into database video

---
+++ WorkoutCreation/WorkoutCreation/MVVM/Models/Exercise.cs
@@ -0,0 +1,18 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkoutCreation.MVVM.Models
+{
+    public class Exercise
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Sets { get; set; }
+        public int Reps { get; set; }
+    }
+}
